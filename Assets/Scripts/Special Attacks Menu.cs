eb1376dd004f9e99d5b5fd15d49744cb006f6d91
Camillo2707 <camillogl@online.de>
2024-12-28 17:23:15 +0100

    More Battle System stuff + basic main menu

---
+++ Assets/Scripts/Special Attacks Menu.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SpecialAttacksMenu : MonoBehaviour
+{
+    public GameObject SpecialAttack1;
+    public GameObject SpecialAttack2;
+    public GameObject SpecialAttack3;
+    public GameObject BackButton;
+    public GameObject Attack;
+    public GameObject SpecialAttackMenu2;
+    public GameObject Items;
+
+    public void SpecialAttackMenu()
+    {
+        SpecialAttack1.SetActive(true);
+        SpecialAttack2.SetActive(true);
+        SpecialAttack3.SetActive(true);
+        BackButton.SetActive(true);
+        Attack.SetActive(false);
+        SpecialAttackMenu2.SetActive(false);
+        Items.SetActive(false);
+    }
+
+    public void LeaveMenu()
+    {
+        SpecialAttack1.SetActive(false);
+        SpecialAttack2.SetActive(false);
+        SpecialAttack3.SetActive(false);
+        BackButton.SetActive(false);
+        Attack.SetActive(true);
+        SpecialAttackMenu2.SetActive(true);
+        Items.SetActive(true);
+    }
+
+    void Start()
+    {
+        SpecialAttack1.SetActive(false);
+        SpecialAttack2.SetActive(false);
+        SpecialAttack3.SetActive(false);
+        BackButton.SetActive(false);
+    }
+}
