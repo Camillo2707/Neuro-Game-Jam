36f8008142a9edaddee5290854d3a486d119a54f
CloudburstSys <cloudburst@cloudburst.lgbt>
2024-12-30 02:59:38 +0000

    Update story view controlling to use new assets

--- Assets/Scripts/Story/StoryController.cs
+++ Assets/Scripts/Story/StoryController.cs
@@ -32,21 +32,27 @@ public class StoryController : MonoBehaviour
         
         // Story segment: FOUND_RECIPE_PRE_BATTLE
         var foundRecipePreBattleStorySegment = new List<StorySegment>();
-        foundRecipePreBattleStorySegment.Add(new StorySegment(true, "Neuro-sama", "neuro-happy", "Hello I am Neuro-sama!", null));
+        foundRecipePreBattleStorySegment.Add(new StorySegment(false, null, null, null, "FOUND_RECIPE_PRE_BOSS"));
+        foundRecipePreBattleStorySegment.Add(new StorySegment(true, "Evil Neuro", "evil-resting", "Hello, I am Evil Neuro.", null));
+        foundRecipePreBattleStorySegment.Add(new StorySegment(true, "Evil Neuro", "evil-resting", "This part of the code tests background image changing", null));
+        foundRecipePreBattleStorySegment.Add(new StorySegment(true, "Evil Neuro", "evil-resting", "As this is something that can happen during the game, it's important that this is fully tested.", "FOUND_RECIPE_PRE_BOSS_2"));
+        foundRecipePreBattleStorySegment.Add(new StorySegment(true, "Evil Neuro", "evil-resting", "As if there are no changes, it will be super super super embarrassing during the voting period of the game jam.", null));
+        foundRecipePreBattleStorySegment.Add(new StorySegment(true, "Evil Neuro", "evil-super-mad", "Fuck Vedal for not bringing him with me though.", "FOUND_RECIPE_BOSS_APPEARS"));
+        foundRecipePreBattleStorySegment.Add(new StorySegment(true, "Evil Neuro", "evil-super-mad", "The boss battle will begin now, after the user interacts one last time.", null));
         
         stories.Add("FOUND_RECIPE_PRE_BATTLE", foundRecipePreBattleStorySegment);
         
         // Story segment: FOUND_RECIPE_POST_BATTLE
         
         var foundRecipePostBattleStorySegment = new List<StorySegment>();
-        foundRecipePostBattleStorySegment.Add(new StorySegment(false, null, null, null, "FOUND_RECIPE_POST_BOSS"));
+        foundRecipePostBattleStorySegment.Add(new StorySegment(false, null, null, null, "FOUND_RECIPE_POST_BOSS", Color.HSVToRGB(0, 0, 95)));
         foundRecipePostBattleStorySegment.Add(new StorySegment(true, "Neuro-sama", "neuro-resting", "Hello I am Neuro-sama!", null));
         foundRecipePostBattleStorySegment.Add(new StorySegment(true, "Neuro-sama", "neuro-sad", "If this doesn't work, I will be sad.", null));
         foundRecipePostBattleStorySegment.Add(new StorySegment(true, "Neuro-sama", "neuro-happy", "But if it does, I will be really happy!", null));
         
         stories.Add("FOUND_RECIPE_POST_BATTLE", foundRecipePostBattleStorySegment);
         
-        /*CallStory("FOUND_RECIPE_POST_BATTLE", (storyId) =>
+        /*CallStory("FOUND_RECIPE_PRE_BATTLE", (storyId) =>
         {
             Debug.Log(storyId);
         });*/
@@ -79,7 +85,10 @@ public class StoryController : MonoBehaviour
         if (currentSegment.IsTalking)
         {
             _containerObj.SetActive(true);
-            _continuePromptObj.GetComponent<TMP_Text>().color = Color.HSVToRGB(0, 0, 95);
+            if (currentSegment.ContinuePromptColor.HasValue)
+                _continuePromptObj.GetComponent<TMP_Text>().color = currentSegment.ContinuePromptColor.Value;
+            else
+                _continuePromptObj.GetComponent<TMP_Text>().color = Color.HSVToRGB(0, 0, 95);
             
             if (currentSegment.TalkingName != null)
                 _characterNameObj.GetComponent<TMP_Text>().text = currentSegment.TalkingName;
@@ -91,7 +100,10 @@ public class StoryController : MonoBehaviour
         else
         {
             _containerObj.SetActive(false);
-            _continuePromptObj.GetComponent<TMP_Text>().color = Color.HSVToRGB(0, 0, 0);
+            if (currentSegment.ContinuePromptColor.HasValue)
+                _continuePromptObj.GetComponent<TMP_Text>().color = currentSegment.ContinuePromptColor.Value;
+            else
+                _continuePromptObj.GetComponent<TMP_Text>().color = Color.HSVToRGB(0, 0, 0);
         }
         
         if (currentSegment.BackgroundImg != null)
@@ -118,13 +130,15 @@ class StorySegment
     [CanBeNull] public string TalkingIcon;
     [CanBeNull] public string TalkingText;
     [CanBeNull] public string BackgroundImg;
+    public Color? ContinuePromptColor;
     
-    public StorySegment(bool isTalking, [CanBeNull] string talkingName, [CanBeNull] string talkingIcon, [CanBeNull] string talkingText, [CanBeNull] string backgroundImg)
+    public StorySegment(bool isTalking, [CanBeNull] string talkingName, [CanBeNull] string talkingIcon, [CanBeNull] string talkingText, [CanBeNull] string backgroundImg, Color? continuePromptColor = null)
     {
         this.IsTalking = isTalking;
         this.TalkingName = talkingName;
         this.TalkingIcon = talkingIcon;
         this.TalkingText = talkingText;
         this.BackgroundImg = backgroundImg;
+        this.ContinuePromptColor = continuePromptColor;
     }
 }
